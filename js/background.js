e042ea4652e35adb6665add687ec91770ec4a6af
Sheldon Soo <hello@sooxt98.space>
2019-09-09 17:18:41 +0800

    clean up

--- js/background.js
+++ js/background.js
@@ -1,14 +1,3 @@
-// webview.addEventListener('loadcommit', function() {
-//     webview.insertCSS({
-//         code: `
-//         ._668f8eb097574762d66d361cc6d37850-scss, .Root__ads-container {
-//             display: none !important;
-//         }
-//         `,
-//         runAt: 'document_start'
-// });
-
-
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) { return {cancel: true}; },
     {urls: ["*://audio-ak-spotify-com.akamaized.net/*"]},
