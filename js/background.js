54c4614459f723c04d42524ea028a38df2a9b89a
sooxt98 <sooxiaotong@gmail.com>
2020-01-23 22:20:06 +0800

    Updates~

--- js/background.js
+++ js/background.js
@@ -2,6 +2,6 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(details) { return {cancel: true}; }, { urls: [
         "*://*.audio-ak-spotify-com.akamaized.net/*",
         "*://*.googlesyndication.com/*",
-        "*://*.audio-akp-quic-spotify-com.akamaized.net/*"
+        "*://*.audio-akp-quic-spotify-com.akamaized.net/*",
         "*://*.gae-spclient.spotify.com/*"
     ]}, ["blocking"])
