9b53fe4c9469b1df4998cd9d15a970d34fc0322e
sooxt98 <hello@sooxt98.space>
2021-02-11 00:44:06 +0800

    Magic ads customisation ✨

--- js/background.js
+++ js/background.js
@@ -1,5 +1,8 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return {cancel: true}; }, { urls: [
+    function(details) { 
+        if(details.url.includes("audio-akp-quic-spotify-com.akamaized.net")) return {redirectUrl: 'https://raw.githubusercontent.com/anars/blank-audio/master/1-second-of-silence.mp3'};
+        return {cancel: true};
+    }, { urls: [
         "*://*.audio-ak-spotify-com.akamaized.net/*",
         "*://*.googlesyndication.com/*",
         "*://*.audio-akp-quic-spotify-com.akamaized.net/*"
