91f7735dda8b3c25a88f87848dc31589d701cc1b
sooxt98 <hello@sooxt98.space>
2021-03-02 00:43:04 +0800

    Magic ads customisation V2 ✨

--- js/background.js
+++ js/background.js
@@ -1,9 +1,4 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { 
-        if(details.url.includes("audio-akp-quic-spotify-com.akamaized.net")) return {redirectUrl: 'https://raw.githubusercontent.com/anars/blank-audio/master/1-second-of-silence.mp3'};
-        return {cancel: true};
-    }, { urls: [
-        "*://*.audio-ak-spotify-com.akamaized.net/*",
-        "*://*.googlesyndication.com/*",
-        "*://*.audio-akp-quic-spotify-com.akamaized.net/*"
-    ]}, ["blocking"])
+chrome.webRequest.onHeadersReceived.addListener(function(details) { 
+    if(details.responseHeaders.find(el => el.name == "Content-Type").value.includes("mp3"))
+        return {redirectUrl: 'https://raw.githubusercontent.com/texnikru/blank-mp3s/master/point1sec.mp3'};
+}, { urls: ["*://*.audio-akp-quic-spotify-com.akamaized.net/*"]}, ["blocking","responseHeaders"])
