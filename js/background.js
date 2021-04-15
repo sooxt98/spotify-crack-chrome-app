5bfcb3c64227a64acff0fcbaa36156bbaf4ccc08
sooxt98 <sooxiaotong@gmail.com>
2021-04-15 14:35:09 +0800

    :sparkles: Added support for Microsoft Edge App

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
 chrome.webRequest.onHeadersReceived.addListener(function(details) { 
     if(details.responseHeaders.find(el => el.name == "Content-Type").value.includes("mp3"))
-        return {redirectUrl: 'https://raw.githubusercontent.com/texnikru/blank-mp3s/master/point1sec.mp3'};
-}, { urls: ["*://*.audio-akp-quic-spotify-com.akamaized.net/*"]}, ["blocking","responseHeaders"])
+        return {redirectUrl: 'https://raw.githubusercontent.com/texnikru/blank-mp3s/master/1sec.mp3'};
+}, { urls: ["*://*.audio-akp-quic-spotify-com.akamaized.net/*", "*://*.audio-fa.scdn.co/*"]}, ["blocking","responseHeaders"])
