41cf6dc217f00f9cdf632f69a482a0ee2159647f
sooxt98 <sooxiaotong@gmail.com>
2023-09-18 21:47:16 +0800

    ✨ Added some sugar

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
 chrome.webRequest.onHeadersReceived.addListener(function(details) { 
     if(details.responseHeaders.find(el => el.name == "Content-Range").value.split("/").pop().length == 6)
         return {redirectUrl: 'https://raw.githubusercontent.com/texnikru/blank-mp3s/master/1sec.mp3'};
-}, { urls: ["*://*.audio-akp-quic-spotify-com.akamaized.net/*", "*://*.audio-fa.scdn.co/*", "*://creativeservice-production.scdn.co/*"]}, ["blocking","responseHeaders"])
+}, { urls: ["*://*.audio-ak-spotify-com.akamaized.net/*", "*://*.audio-akp-quic-spotify-com.akamaized.net/*", "*://*.audio-fa.scdn.co/*", "*://creativeservice-production.scdn.co/*"]}, ["blocking","responseHeaders"])
